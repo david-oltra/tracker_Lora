b47488c7d5acb6a8de7bc567309dfdae43745005
david <davoltra@gmail.com>
2024-02-13 17:11:29 +0100

    Delete files

--- src/lorakeys.h
+++
@@ -1,9 +0,0 @@
-
-// Application in string format.
-// For TTN issued EUIs the first bytes should be 70B3D5
-constexpr char const appEui[] = "70B3D51403198612";
-
-// Device EUI in string format.
-constexpr char const devEui[] = "70B3D57ED0064C6A";
-// Application key in string format.
-constexpr char const appKey[] = "6951564AD3A9BA5766D85631C2F78FAE";
